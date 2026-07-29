3740893201f8cc74eb432286330692160f99436d
sajtazim991-beep <sajtazim991@gmail.com>
2026-07-29 16:33:28 +0500

    Replace startAI function with askAI function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function startAI() {
-    document.getElementById("box").innerHTML =
-    "✨ AI запущен! Чем я могу помочь?";
+function askAI() {
+    let question = document.getElementById("question").value;
+
+    if(question == "") {
+        document.getElementById("box").innerHTML =
+        "Напиши сначала вопрос 🙂";
+    } else {
+        document.getElementById("box").innerHTML =
+        "Ты спросил: " + question + "<br><br>🤖 Я пока учусь отвечать!";
+    }
 }
